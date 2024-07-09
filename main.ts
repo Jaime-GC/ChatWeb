6c281a78c061c1395baac77f59185287e6fd295b
Jaime González Carbajo <jgcarbajo1@gmail.com>
2024-07-09 19:41:24 +0200

    ChatWeb Version Inicial

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
-import express, { Application, Request, Response } from "npm:express@4.18.2";
-import process from "node:process";
+import express, { Application, Request, Response } from "express";
 import http from "node:http";
+import path from "node:path";
+import process from "node:process";
+import fs from "node:fs";
 import { Server, Socket } from "socket.io";
 import logger from "morgan";
 import { createClient } from "@libsql/client";
@@ -8,15 +10,25 @@ import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 
 const env = config();
-
 const port: number = 3000;
 
 const app: Application = express();
 
-
 app.use(logger('dev'));
-app.get('/', (_req: Request, res: Response) => {
-  res.sendFile(process.cwd() + "/index.html");
+
+app.get('/', async (_req: Request, res: Response) => {
+  try {
+    const filePath = path.resolve(process.cwd(), "index.html");
+    const fileInfo = await fs.promises.stat(filePath);
+    if (fileInfo.isFile()) {
+      res.sendFile(filePath);
+    } else {
+      res.status(404).send('File not found');
+    }
+  } catch (error) {
+    console.error('Error fetching index.html', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 const httpserver: http.Server = http.createServer(app);
@@ -36,7 +48,7 @@ await db.execute(`
     message TEXT,
     time TEXT
   )
-`)
+`);
 
 io.on('connection', async (socket: Socket) => {
   console.log('a user connected');
@@ -46,28 +58,26 @@ io.on('connection', async (socket: Socket) => {
   });
 
   socket.on('chat message', async ({ user, message }) => {
-  
-  const time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
-  let result;
+    let result;
 
-  try {
-    result = await db.execute({  
-      sql: 'INSERT INTO messages (user, message, time) VALUES (:usuario, :mensaje, :tiempo)',
-      args: {
-        usuario:user, 
-        mensaje:message, 
-        tiempo:time
-      },
-    });
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (user, message, time) VALUES (:usuario, :mensaje, :tiempo)',
+        args: {
+          usuario: user,
+          mensaje: message,
+          tiempo: time
+        },
+      });
 
-  } catch (error) {
-    console.error('Error inserting message', error);
-    return;
-  }
+    } catch (error) {
+      console.error('Error inserting message', error);
+      return;
+    }
 
-  io.emit('chat message', { user, message, time }, result.lastInsertRowid?.toString());
-    
+    io.emit('chat message', { user, message, time }, result.lastInsertRowid?.toString());
   });
 
   if (!socket.recovered) {
@@ -79,13 +89,13 @@ io.on('connection', async (socket: Socket) => {
 
       results.rows.forEach((row) => {
         if (row.id !== null) {
-          socket.emit('chat message', 
-          {
-            user: row.user,
-            message: row.message,
-            time: row.time,
-          }, 
-          row.id.toString());
+          socket.emit('chat message',
+            {
+              user: row.user,
+              message: row.message,
+              time: row.time,
+            },
+            row.id.toString());
         }
       });
 
